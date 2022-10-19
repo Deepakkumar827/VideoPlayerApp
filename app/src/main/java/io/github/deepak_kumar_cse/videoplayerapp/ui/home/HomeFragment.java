254282ee565293627d19c11086248b43de0c52ad
Deeepak Kumar <deepakkumar82753@gmail.com>
2022-10-19 21:08:50 +0530

    some changes

--- app/src/main/java/io/github/deepak_kumar_cse/videoplayerapp/ui/home/HomeFragment.java
+++ app/src/main/java/io/github/deepak_kumar_cse/videoplayerapp/ui/home/HomeFragment.java
@@ -7,13 +7,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import io.github.deepak_kumar_cse.videoplayerapp.databinding.FragmentHomeBinding;
-import io.github.deepak_kumar_cse.videoplayerapp.filemanager.FileManagerActivity;
 
 public class HomeFragment extends Fragment {
 
@@ -32,8 +32,6 @@ public class HomeFragment extends Fragment {
         binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getActivity(), FileManagerActivity.class);
-                startActivity(intent);
             }
         });
 
