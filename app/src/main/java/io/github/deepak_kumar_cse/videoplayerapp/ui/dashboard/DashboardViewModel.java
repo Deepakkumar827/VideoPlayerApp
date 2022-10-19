6418c12a4681978d1423e55c4f2c7a69bd6cfaa3
Deeepak Kumar <deepakkumar82753@gmail.com>
2022-10-19 18:10:32 +0530

    initial commit

---
+++ app/src/main/java/io/github/deepak_kumar_cse/videoplayerapp/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package io.github.deepak_kumar_cse.videoplayerapp.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
